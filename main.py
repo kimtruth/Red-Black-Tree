5f9dd726d1b136d1b5a2df6b1a6ac62a32b4a0c6
j28150 <j28150@naver.com>
2017-06-09 18:41:40 +0900

    5th homework..

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
+import os
 from Tree import RBT
 
 rbt = RBT()
 
-filename = input('filename= ')
-txt = open(filename).read()
-datas = list(map(int, txt.split('\n')))
+filenames = os.listdir('./input/')
+for filename in filenames:
+	print ('filename=', filename)
+	txt = open('./input/' + filename).read()
+	datas = list(map(int, txt.split('\n')))
 
-insert = 0
-delete = 0
-miss = 0
-for data in datas:
-	if data > 0:
-		rbt.insert(data)
-		insert += 1
-	elif data < 0:
-		n = rbt.search(-data)
-		if rbt.delete(n) == -1:
-			miss += 1
+	insert = 0
+	delete = 0
+	miss = 0
+	for data in datas:
+		if data > 0:
+			rbt.insert(data)
+			insert += 1
+		elif data < 0:
+			n = rbt.search(-data)
+			if rbt.delete(n) == -1:
+				miss += 1
+			else:
+				delete += 1
 		else:
-			delete += 1
-	else:
-		break
+			break
 
-print('total =', rbt.node_count())
-print('insert =', insert)
-print('deleted =', delete)
-print('miss = ', miss)
-print('nb =', rbt.black_node_count())
-print('bh =', rbt.black_height())
+	print('total =', rbt.node_count())
+	print('insert =', insert)
+	print('deleted =', delete)
+	print('miss = ', miss)
+	print('nb =', rbt.black_node_count())
+	print('bh =', rbt.black_height())
 
-rbt.inorder(rbt.root)
+	rbt.inorder(rbt.root)
